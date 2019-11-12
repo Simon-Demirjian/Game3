92d1de87475b8af3d6a417b5500e8c0bb817b42d
Simon Demirjian <sad5961@g.rit.edu>
2019-11-12 12:20:03 -0500

    started base of interacting manager aaaeeeaooauuaueueuua

---
+++ TIt Anic/Assets/Scripts/InteractingManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractingManager : MonoBehaviour
+{
+    public GameObject player;
+    public List<string> evidenceCollected;
+
+    private GameObject[] containers;
+    private List<GameObject> containerList;
+    private GameObject[] doors;
+    private List<GameObject> doorList;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //generate container list based on tag
+        containers = GameObject.FindGameObjectsWithTag("Container");
+        foreach(GameObject container in containers)
+        {
+            containerList.Add(container);
+        }
+
+        //generate door list based on tag
+        doors = GameObject.FindGameObjectsWithTag("Door");
+        foreach(GameObject door in doors)
+        {
+            doorList.Add(door);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
