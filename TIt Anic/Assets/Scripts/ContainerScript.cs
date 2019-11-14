b7aa33bead1da407c5dd1e899c6d828bf8654e80
Simon Demirjian <sad5961@g.rit.edu>
2019-11-14 12:18:00 -0500

    finished first stage of interacting manager, made door and container script

---
+++ TIt Anic/Assets/Scripts/ContainerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerScript : MonoBehaviour
+{
+    public bool open;
+    public bool contains;
+    public bool containsEvidence;
+    public string item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        open = false;
+        contains = false;
+        containsEvidence = false;
+        item = "test item";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
